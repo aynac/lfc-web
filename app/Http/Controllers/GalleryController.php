0c86d530cdd7e3090c22e36950cba03dbf4c76fa
sreykalg <lengsreyka.apple@gmail.com>
2025-12-17 08:41:08 +0700

    [UPDATE] Gallery

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -8,13 +8,6 @@ class GalleryController extends Controller
 {
     public function gallery()
     {
-        // $gallery = [
-        //     'hun_sen_cup' => Storage::disk('public')->files('gallery/HunSen_cup_23'),
-        //     'pp_trip' => Storage::disk('public')->files('gallery/pp_trip'),
-        //     'prop_p' => Storage::disk('public')->files('gallery/cpl'),
-        //     'team_dinner' => Storage::disk('public')->files('gallery/team_dinner'),
-        //     'match_day' => Storage::disk('public')->files('gallery/slide_pic'),
-        // ];
         $categories = [
             'hun_sen_cup' => 'gallery/HunSen_cup_23',
             'pp_trip'    => 'gallery/pp_trip',
@@ -31,7 +24,6 @@ class GalleryController extends Controller
             $gallery[$key] = array_filter($files, fn($file) => preg_match('/\.(jpe?g|png|webp|gif)$/i', $file));
         }
         
-
        return view('user.gallery', compact('gallery'));
     }
 }
